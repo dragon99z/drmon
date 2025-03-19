6003d067556c6e8aa616b3e989622cdbdb918327
dragon99z <dragon99@online.de>
2025-03-19 23:46:37 +0100

    Update f.lua

--- lib/f.lua
+++ lib/f.lua
@@ -1,13 +1,30 @@
+direct_connect = { "right", "left", "back", "top", "bottom" }
+
+function inTable(tbl, item)
+    for key, value in pairs(tbl) do
+        if value == item then return key end
+    end
+    return false
+end
 
 -- peripheral identification
 --
-function periphSearch(type)
+function periphSearch(type,direct)
    local names = peripheral.getNames()
    local i, name
    for i, name in pairs(names) do
-      if peripheral.getType(name) == type then
-         return peripheral.wrap(name)
-      end
+      if direct then
+        if inTable(direct_connect,name) then
+          if peripheral.getType(name) == type then
+              return peripheral.wrap(name)
+          end
+        end
+      else
+        if not inTable(direct_connect,name) then
+          if peripheral.getType(name) == type then
+              return peripheral.wrap(name)
+          end
+        end
    end
    return null
 end
